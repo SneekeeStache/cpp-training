79696a99e0e1d2a06ae4a0835801530d1a6f7b62
SneekeeStache <kyle.prudhomme@proton.me>
2023-03-27 11:10:20 +0200

    types de variable et references

--- test.cpp
+++ test.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
-
-int main(){
+#include <string>
+int main()
+{
+    //permet d'afficher du text
     std::cout << "hello world" << std::endl;
+    //variable int
+    int monInt(15);
+    //variable String
+    std::string monString("test");
+    //affiche mon int et mon string
+    std::cout << "mon int:" << monInt << " mon String:" << monString << std::endl;
+    //reference mon int
+    int& maReferenceInt(monInt);
+    std::cout << "mon int comme reference:" << maReferenceInt << std::endl;
     return 0;
 }
